40203ed82e63cb8652d9126d7c6c078161b14dd6
schubertjan <honza.schubert@gmail.com>
2020-10-28 08:43:05 +0100

    fix utils.r

--- utils_cviceni4.r
+++ utils_cviceni4.r
@@ -1,6 +1,6 @@
 cviceni <- 1:10
 datum <- seq.Date(as.Date("2020-10-06"), length.out = 10, by = "week")
 
-write.table(data.frame(cviceni, datum), sep = ";", row.names = FALSE, file = "~/MaL/Cviceni 4/data_prostrednik.csv")
+write.table(data.frame(cviceni, datum), sep = ";", row.names = FALSE, file = "~/MaL/Cviceni 4/data_strednik.csv")
 
 
